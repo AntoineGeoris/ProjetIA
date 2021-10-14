35a0a68d7a85cd26def09a5a271c8409e2f9f71b
Detinne Natan <81631285+Natan-58@users.noreply.github.com>
2021-10-14 20:28:05 +0200

    Update Plateau de jeu JS
    Création du plateau de jeu grâce à JS
    
    Co-Authored-By: Mathieu Kuczerowski <82814479+KucMa@users.noreply.github.com>

---
+++ myapp/static/scripts/game_board.js
@@ -0,0 +1,22 @@
+const GAME_BOARD_HEIGHT = 5;
+const GAME_BOARD_WIDTH = 5;
+
+function createGameBoard() {
+    document.write('<table id="gameBoard">');
+        let gameBoard = [];
+        for(var iLine = 0; iLine < GAME_BOARD_HEIGHT; iLine++)
+        {
+            document.write('<tr>');
+            gameBoard.push([]);
+            for(var iBox = 0; iBox < GAME_BOARD_WIDTH; iBox++)
+            {
+                let box = document.createElement("td");
+                box.classList.add("gameBoardBox")
+                document.body.getElementsByTagName("tr")[iLine].appendChild(box);
+                gameBoard[iLine].push(box);
+                document.write('</td>');
+            }
+			document.write('</tr>');
+		}
+	document.write('</table>');
+}  
