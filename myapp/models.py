d1304b78e9a3e33576beb7759bc64e08087d1ae7
Mathieu Kuczerowski <kuczerowski.mathieu@gmail.com>
2021-10-25 11:57:16 +0200

    update models.py
    Co-Authored-By: Detinne Natan <81631285+Natan-58@users.noreply.github.com>

--- myapp/models.py
+++ myapp/models.py
@@ -22,16 +22,14 @@ class Player(db.Model):
 	image_file = db.Column(db.String(20), nullable = False, default = 'default.jpg')
 	password =  db.Column(db.String(60), nullable = False)
 	games = db.relationship('Game', backref='Participant')
-	moves = db.relationship('Move', backref='Initiator')
 
 	def __repr__(self) :
 		return f"User('{self.username}, {self.email}, {self.image_file}')"
 
-class Move(db.Model):	
-	x_move = db.Column(db.Integer, primary_key = True)
-	y_move = db.Column(db.Integer, primary_key = True)
-	no_turn = db.Column(db.Integer, primary_key = True)
-	player_id = db.Column(db.Integer, db.ForeignKey('player.id'), nullable = False)
+class Cell(db.Model):	
+	x_coord = db.Column(db.Integer, primary_key = True)
+	y_coord = db.Column(db.Integer, primary_key = True)
+	owner = db.Column(db.Integer, db.ForeignKey('player.id'), nullable = True) #IA en tant qu'agent joueur dans la DB ? (ID, username)
 	game_id = db.Column(db.Integer, db.ForeignKey('game.id'), nullable = False)
 
 
@@ -39,8 +37,10 @@ class Game(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	id_winner = db.Column(db.Integer, nullable = True)
 	date_played = db.Column(db.DateTime, nullable = False, default = datetime.utcnow) #no parenthesis, we want to pass the function as an argument, not having the current time (when 'executed') 
+	player_1_pos = db.Column(db.Integer, nullable = False)
+	player_2_pos = db.Column(db.Integer, nullable = False)
 	player_id = db.Column(db.Integer, db.ForeignKey('player.id'), nullable = False)
-	moves = db.relationship('Move', backref='Initiator')
+	cells = db.relationship('Cell', backref='Contains')
 
 	def __repr__(self) :
 		return f"Game('{self.id}, {self.id_winner}, {self.date_played}')"
