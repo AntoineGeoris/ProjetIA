a178bcf89e6e998979421ff09e001f49f64bfaaf
Detinne Natan <81631285+Natan-58@users.noreply.github.com>
2021-11-02 15:56:14 +0100

    OWL affichage du plateau

--- myapp/static/scripts/game.js
+++ myapp/static/scripts/game.js
@@ -1,147 +1,58 @@
-const GAME_BOARD_WIDTH = 5;
-const GAME_BOARD_HEIGHT = 5;
+(function () {
+    const { Component, Store, mount } = owl;
+    const { xml } = owl.tags;
+    const { whenReady } = owl.utils;
+    const { useRef, useDispatch, useState, useStore } = owl.hooks;
 
-class Player {
-    constructor(name, line, box) {
-        this._name = name;
-        this._line = line;
-        this._box = box;
-    }
+    const GAME_BOARD_TEMPLATE = xml /* xml */ `
+        <div>
+            <table id="gameBoard">
+                <t t-foreach="table" t-as="line">
+                    <tr>
+                        <t t-foreach="line" t-as="box">
+                            <td class="gameBoardBox"></td>
+                        </t>
+                    </tr>
+                </t>
+            </table>
+            <div id="arrow_div">
+                <a class="arrow" id="left" t-on-click="arrowClick('left')"></a>
+                <a class="arrow" id="right" t-on-click="arrowClick('right')"></a>
+                <a class="arrow" id="up" t-on-click="arrowClick('up')"></a>
+                <a class="arrow" id="down" t-on-click="arrowClick('down')"></a>
+            </div>
+        </div>`;
 
-    /*---getters---*/
+    class GameBoard extends Component {
+        static template = GAME_BOARD_TEMPLATE;
 
-    get name() {
-        return this._name;
-    }
+        table = [[0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]];
 
-    get line() {
-        return this._line;
-    }
+        async arrowClick(movement) {
+            let move = false;
 
-    get box() {
-        return this._box;
-    }
+            if (movement === "left")
+                move = movement;
+            else if (movement === "right")
+                move = movement;
+            else if (movement === "up")
+                move = movement;
+            else if (movement === "down")
+                move = movement;
 
-    /*---setters---*/
-
-    set line(line) {
-        if (line < GAME_BOARD_WIDTH && line >= 0) {
-            this._line = line;
-        } else {
-            throw "Position invalide."
-        }
-    }
-
-    set box(box) {
-        if (box < GAME_BOARD_HEIGHT && box >= 0) {
-            this._box = box;
-        } else {
-            throw "Position invalide."
-        }
-    }
-
-    set name(name) {
-        this._name = name;
-    }
-}
-
-class GameBoard {
-    constructor(player1, player2) {
-        this.player1 = player1;
-        this.player2 = player2;
-        this.players = [player1, player2];
-        this.grid = [];
-        this.p = 0;
-    }
-
-    display() {
-        let directions = ["down", "up", "left", "right"];
-        document.write('<table id="gameBoard">');
-
-        for (let iLine = 0; iLine < GAME_BOARD_HEIGHT; iLine++) {
-            document.write('<tr>');
-            this.grid.push([]);
-            for (let iBox = 0; iBox < GAME_BOARD_WIDTH; iBox++) {
-                let box = document.createElement("td");
-                box.classList.add("gameBoardBox")
-                document.body.getElementsByTagName("tr")[iLine].appendChild(box);
-                this.grid[iLine].push(box);
-                document.write('</td>');
+            if (move) {
+                console.log(move);
             }
-            document.write('</tr>');
         }
-        this.grid[0][0].classList.add("playerOne")
-        this.grid[0][0].classList.toggle("playerOn");
-        this.grid[4][4].classList.add("playerTwo")
-        this.grid[4][4].classList.toggle("playerOn");
-        document.write('</table>');
-
-        document.write("<div id='arrow_div'>");
-        for (let i = 0; i < 4; i++) {
-            let arrow = document.createElement("a");
-            arrow.classList.add("arrow");
-            arrow.id = directions[i];
-            arrow.addEventListener('click', () => this.move(arrow.id));
-            document.getElementById("arrow_div").appendChild(arrow);
-        }
-        document.write("</div>");
     }
 
-    move(direction) {
-        if (this.#allowedMove(direction)) {
-            let box = this.players[this.p % 2].box;
-            let line = this.players[this.p % 2].line;
-
-            this.grid[line][box].classList.toggle("playerOn");
-            
-            switch(direction){
-                case "left":
-                    this.grid[line][box - 1].classList.add(this.p % 2 == 0 ? "playerOne" : "playerTwo");
-                    this.grid[line][box - 1].classList.toggle("playerOn");
-                    this.players[this.p % 2].box--;
-                    break;
-                case "right":
-                    this.grid[line][box + 1].classList.add(this.p % 2 == 0 ? "playerOne" : "playerTwo");
-                    this.grid[line][box + 1].classList.toggle("playerOn");
-                    this.players[this.p % 2].box++;
-                    break;
-                case "down":
-                    this.grid[line + 1][box].classList.add(this.p % 2 == 0 ? "playerOne" : "playerTwo");
-                    this.grid[line + 1][box].classList.toggle("playerOn");
-                    this.players[this.p % 2].line++;
-                    break;
-                default:
-                    this.grid[line - 1][box].classList.add(this.p % 2 == 0 ? "playerOne" : "playerTwo");
-                    this.grid[line - 1][box].classList.toggle("playerOn");
-                    this.players[this.p % 2].line--;
-                    break;
-            }
-            this.p++;
-        }
-        else
-            alert("Déplacement non autorisé");
+    function setup() {
+        owl.config.mode = "dev";
+        mount(GameBoard, { target: document.body });
     }
 
-    #allowedMove(direction) {
-        let box = this.players[this.p % 2].box;
-        let line = this.players[this.p % 2].line;
+    whenReady(setup);
+    
+}) ();
 
-        if (direction == "left") 
-            return box - 1 >= 0 && !this.grid[line][box - 1].classList.contains(this.p % 2 != 0 ? "playerOne" : "playerTwo");
 
-        if (direction == "right") 
-            return box + 1 < GAME_BOARD_WIDTH && !this.grid[line][box + 1].classList.contains(this.p % 2 != 0 ? "playerOne" : "playerTwo");
-
-        if (direction == "down") 
-            return line + 1 < GAME_BOARD_HEIGHT && !this.grid[line + 1][box].classList.contains(this.p % 2 != 0 ? "playerOne" : "playerTwo");
-
-        return line - 1 >= 0 && !this.grid[line - 1][box].classList.contains(this.p % 2 != 0 ? "playerOne" : "playerTwo");
-    }
-}
-
-/* window.onload = function () {
-    let player1 = new Player("Natan", 0, 0, "#2ACAEA");
-    let player2 = new Player("Antoine", 4, 4, "#F62B2B");
-    let game_board = new GameBoard(player1, player2);
-    game_board.display();
-} */
