d7e639c9700159645022b196c56da0a47e0381ec
Detinne Natan <81631285+Natan-58@users.noreply.github.com>
2021-11-02 19:24:06 +0100

    DB Update

--- myapp/models.py
+++ myapp/models.py
@@ -6,6 +6,12 @@ import logging as lg
 def init_db() :
 	db.drop_all()
 	db.create_all()
+	players = [
+		Player(email = "natan@email.be", username = "Natan", password = "1235"),
+		Player(email = "mathieu@email.be", username = "Mathieu", password = "1235"),
+		Player(email = "antoine@email.be", username = "Antoine", password = "1235"),
+	]
+	db.session.add_all(players)
 	db.session.commit()
 	lg.warning('Database initialized !') 
 
@@ -13,30 +19,15 @@ def init_db() :
 
 class GameBoard(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
-	player_1_pos = db.Column(db.String(2), nullable = False)
-	player_2_pos = db.Column(db.String(2), nullable = False)
+	player_1_pos = db.Column(db.String(2), nullable = False, default = "00")
+	player_2_pos = db.Column(db.String(2), nullable = False, default = "44")
 	date_played = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
-	no_turn = db.Column(db.Integer, nullable = False, default = 1)
+	no_turn = db.Column(db.Integer, nullable = False, default = "1")
 	cells = db.Column(db.String(25), nullable = False, default = "1000000000000000000000002")
 
-	player_1_id = db.Column(db.Integer, db.ForeignKey('player.id'))
-	player_1 = db.relationship('Player', backref = db.backref('player_1'))
-	player_2_id = db.Column(db.Integer, db.ForeignKey('player.id'))
-	player_2 = db.relationship('Player', backref = db.backref('player_2'))
+	player_1_id = db.Column(db.Integer, db.ForeignKey('player.id'), nullable = True)
+	player_2_id = db.Column(db.Integer, db.ForeignKey('player.id'), nullable = True)
 
-	def __init__(self, player_1 = None, player_2 = None):
-		self.player_1 = player_1
-		self.player_2 = player_2
-		self.player_1_pos = "00"
-		self.player_2_pos = "44"
-		self.no_turn = 1
-		self.cells = [
-				[1,0,0,0,0],
-				[0,0,0,0,0],
-				[0,0,0,0,0],
-				[0,0,0,0,0],
-				[0,0,0,0,2]
-			]
 
 class Player(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
@@ -45,10 +36,7 @@ class Player(db.Model):
 	password = db.Column(db.String(20), nullable = False)
 	#image_file = db.Column(db.String(30), nullable = False)
 
-	def __init__(self, email, username, password):
-		self.email = email
-		self.username = username
-		self.password = password
+	#games = db.relationship('GamBoard', backref = db.backref('player') , lazy = True)
 
 	def __repr__(self):
-		return f"User : ('{self.id}') '{self.username}'"
+		return f"User : ({self.id}) {self.username}"
