3e27b3bd38931eef9d2ef33663717ae81183ee81
Detinne Natan <81631285+Natan-58@users.noreply.github.com>
2021-12-03 12:28:58 +0100

    Doc

--- myapp/ia.py
+++ myapp/ia.py
@@ -6,6 +6,15 @@ from myapp import db
 class AI:
 	MOVES = ["down", "up", "left", "right"]
 
+	#Description:
+	#	This function gives the next move of the AI and update the QTacle
+	#Preconditions : 
+	# 	- game_board is an instnace from the GameBoard class
+	#   - board is the state from the game_board in array format 
+	# 	- eps >= 0 and eps <= 1
+	#Postconditions :
+	#	- return the move from the AI
+	# 	- QTable score are updated
 	def get_move(self, game_board, board, eps):
 		state = game_board.board + game_board.player_1_pos + game_board.player_2_pos + str(game_board.active_player)
 		move = self.get_action(state, eps)
@@ -23,6 +32,14 @@ class AI:
 
 		return move;
 
+
+	#Description:
+	#	This function looks in the QTable for the best move the AI can make in the given state (exploitation) or a random move (exploration).
+	#Preconditions : 
+	# 	- state is the actual state from the game
+	# 	- eps >= 0 and eps <= 1
+	#Postconditions :
+	#	- return the action from the AI
 	def get_action(self, state, eps):
 		score = models.QTableState.query.get(state)
 		score_is_none = score is None
@@ -40,6 +57,16 @@ class AI:
 
 		return action
 
+	#Description:
+	#	This function calculate the reward gain or lose. (During the game)
+	#	One captured cell = 1 point
+	# 	The reward is equal to the difference of captured squares between the two players.
+	#Preconditions : 
+	# 	- state is the actual state from the game
+	# 	- old_state is the last state where the active player made a move.
+	# 	- active_player is the number of the player who made the move (1 or 2).
+	#Postconditions :
+	#	- return the reward abtain by the active player negative or positive.
 	def reward(self, active_player, state, old_state):
 		player_1_reward = state[0:25].count("1") - old_state[0:25].count("1")
 		player_2_reward = state[0:25].count("2") - old_state[0:25].count("2")
@@ -49,6 +76,17 @@ class AI:
 		else:
 			return player_2_reward - player_1_reward
 
+	
+	#Description:
+	#	This function update the QTable's score from the corresponding state and action.
+	#Preconditions : 
+	# 	- reward of the action
+	# 	- state of the gameBoard of the initial mov
+	#	- state_p1 state of the gameBaord after the move
+	# 	- action initial action 
+	# 	- action_p1 next_action
+	#Postconditions :
+	#	- The QTable is update.
 	def updateQTable(self, reward, state_p1, state, action, action_p1):
 		score = models.QTableState.query.get(state)
 		score_p1 = models.QTableState.query.get(state_p1)
@@ -78,6 +116,17 @@ class AI:
 
 		db.session.commit()
 
+	#Description:
+	#	This function calculate the reward gain or lose. (At the end of the game)
+	# 	Winner reward is egal to his own score.
+	# 	loser reward is egal to the opposie of the winner's score
+	#Preconditions : 
+	# 	- game_board is an instnace from the GameBoard class and the the active player must be the winner
+	#	- winner_score is the score of the winner's score
+	# 	- winner_move is the move of the winner'
+	# 	- player_1_is_ia, player_2_is_ia are set to false if player are human
+	#Postconditions :
+	#	- The QTable is update
 	def end_game(self, game_board, winner_score, winner_move, player_1_is_ia = True, player_2_is_ia = True):
 		winner = game_board.active_player
 		loser = 1 if winner == 2 else 2
